0f4e8e3bdf40cd1a71dde55a6b7ff1b84acfde97
Julien QUENTIN <jqntn88@gmail.com>
2024-01-29 01:14:20 +0100

    init

---
+++ timetracker/timetracker-mingui/src/main.rs
@@ -0,0 +1,52 @@
+#![windows_subsystem = "windows"]
+
+extern crate native_windows_derive as nwd;
+extern crate native_windows_gui as nwg;
+
+use nwd::NwgUi;
+use nwg::NativeUi;
+use winapi::um::winuser::SetProcessDPIAware;
+
+#[derive(Default, NwgUi)]
+pub struct BasicApp {
+    #[nwg_control(size: (300, 135), position: (300, 300), title: "Basic example", flags: "WINDOW|VISIBLE")]
+    #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
+    window: nwg::Window,
+
+    #[nwg_control(text: "Heisenberg", size: (280, 35), position: (10, 10), focus: true)]
+    name_edit: nwg::TextInput,
+
+    #[nwg_control(text: "Say my name", size: (280, 70), position: (10, 50))]
+    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
+    hello_button: nwg::Button,
+}
+
+impl BasicApp {
+    fn say_hello(&self) {
+        nwg::modal_info_message(
+            &self.window,
+            "Hello",
+            &format!("Hello {}", self.name_edit.text()),
+        );
+    }
+
+    fn say_goodbye(&self) {
+        nwg::modal_info_message(
+            &self.window,
+            "Goodbye",
+            &format!("Goodbye {}", self.name_edit.text()),
+        );
+        nwg::stop_thread_dispatch();
+    }
+}
+
+fn main() {
+    unsafe { SetProcessDPIAware() };
+
+    nwg::init().expect("Failed to init Native Windows GUI");
+
+    let _font = nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+    let _app = BasicApp::build_ui(Default::default()).expect("Failed to build UI");
+
+    nwg::dispatch_thread_events();
+}
