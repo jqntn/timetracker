6779070ad0dcfad84fc3463f16f6a2f910ee7a42
Julien QUENTIN <jqntn88@gmail.com>
2024-02-02 01:35:21 +0100

    add new window

--- timetracker/timetracker/src/main.rs
+++ timetracker/timetracker/src/main.rs
@@ -6,26 +6,32 @@ extern crate native_windows_gui as nwg;
 
 use nwd::NwgUi;
 use nwg::NativeUi;
-use winapi::um::winuser::SetProcessDPIAware;
+use winapi::{
+    shared::windef::HWND,
+    um::winuser::{SetForegroundWindow, SetProcessDPIAware},
+};
 
 #[derive(Default, NwgUi)]
 pub struct SystemTray {
     #[nwg_control]
     window: nwg::MessageWindow,
 
-    #[nwg_resource(source_file: Some("res/icon/hourglass-96.ico"))]
+    #[nwg_resource(source_file: Some("res/icon/timetracker-96.ico"))]
     icon: nwg::Icon,
 
     #[nwg_control(icon: Some(&data.icon), tip: Some("timetracker"))]
-    #[nwg_events(MousePressLeftUp: [SystemTray::show_menu], OnContextMenu: [SystemTray::show_menu])]
+    #[nwg_events(MousePressLeftUp: [SystemTray::show_window], OnContextMenu: [SystemTray::show_menu])]
     tray: nwg::TrayNotification,
 
     #[nwg_control(parent: window, popup: true)]
     tray_menu: nwg::Menu,
 
-    #[nwg_control(parent: tray_menu, text: "Open")]
+    #[nwg_control(parent: tray_menu, text: "Show timetracker")]
     #[nwg_events(OnMenuItemSelected: [SystemTray::show_window])]
     tray_item_open: nwg::MenuItem,
+
+    #[nwg_control(parent: tray_menu)]
+    separator: nwg::MenuSeparator,
 
     #[nwg_control(parent: tray_menu, text: "Exit")]
     #[nwg_events(OnMenuItemSelected: [SystemTray::exit])]
@@ -39,20 +45,60 @@ impl SystemTray {
     }
 
     fn show_window(&self) {
-        let _: nwg::MessageChoice = nwg::simple_message("Hello", "Hello, World!");
+        if !self.try_set_foreground_window() {
+            return;
+        }
+
+        let _basic_app: basic_app_ui::BasicAppUi =
+            BasicApp::build_ui(Default::default()).expect("Failed to build UI");
+
+        unsafe { WINDOW_HANDLE = Some(_basic_app.window.handle.hwnd().unwrap()) };
+        let _: bool = self.try_set_foreground_window();
+
+        nwg::dispatch_thread_events();
+    }
+
+    fn try_set_foreground_window(&self) -> bool {
+        if let Some(hwnd) = unsafe { WINDOW_HANDLE } {
+            let _: i32 = unsafe { SetForegroundWindow(hwnd) };
+            return false;
+        }
+        true
     }
 
     fn exit(&self) {
         nwg::stop_thread_dispatch();
+        nwg::stop_thread_dispatch();
     }
 }
+
+#[derive(Default, NwgUi)]
+pub struct BasicApp {
+    #[nwg_control(size: (WINDOW_SIZE.0 , WINDOW_SIZE.1), position: ((nwg::Monitor::width() - WINDOW_SIZE.0) / 2, (nwg::Monitor::height() - WINDOW_SIZE.1) / 2), title: "timetracker", icon: Some(&data.icon))]
+    #[nwg_events( OnWindowClose: [BasicApp::exit] )]
+    window: nwg::Window,
+
+    #[nwg_resource(source_file: Some("res/icon/timetracker-96.ico"))]
+    icon: nwg::Icon,
+}
+
+impl BasicApp {
+    fn exit(&self) {
+        unsafe { WINDOW_HANDLE = None };
+
+        nwg::stop_thread_dispatch();
+    }
+}
+
+const WINDOW_SIZE: (i32, i32) = (800, 450);
+static mut WINDOW_HANDLE: Option<HWND> = None;
 
 fn main() {
     let _: i32 = unsafe { SetProcessDPIAware() };
 
     nwg::init().expect("Failed to init Native Windows GUI");
 
-    let _ui: system_tray_ui::SystemTrayUi =
+    let _system_tray: system_tray_ui::SystemTrayUi =
         SystemTray::build_ui(Default::default()).expect("Failed to build UI");
 
     nwg::dispatch_thread_events();
